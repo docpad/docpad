c067aff0801e72cd71294bb9f7302d2fde9fd4a7
Benjamin "balupton" Lupton <contact@balupton.com>
2011-03-23 20:26:56 +0100

    Markdown files are now previewed

--- server.js
+++ server.js
@@ -1,18 +1,1 @@
-var Hyde = {
-
-	/**
-	 * Initialise Server
-	 */
-	init: function(){
-
-		var app = express.createServer();
-
-		app.get('/', function(req, res){
-				res.send('Hello World');
-		});
-
-		app.listen(3000);
-
-	}
-
-};
+require(__dirname+'/lib/hyde.js');
