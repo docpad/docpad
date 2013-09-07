docpad/docpad

commit 0913cae149e3268ab433c969303eeea251551fef

attempt at removing all process.exit calls, it should happen naturally as if docpad is included as a module, then we would kill the includer

src/lib/testers.coffee
@@ -24,7 +24,6 @@ class PluginTester
 		testerName: null
 		pluginName: null
 		pluginPath: null
-		autoExit: true
 		testPath: null
 		outExpectedPath: null
 		removeWhitespace: false
@@ -75,7 +74,8 @@ class PluginTester
 		joe.describe @config.testerName, (suite,task) ->
 			tester.describe = tester.suite = suite
 			tester.it = tester.test = task
-			tester.done = tester.exit = -> # b/c
+			tester.done = tester.exit = ->
+				tester.docpad?.destroy()
 			next?(null, tester)
 
 		# Chain
@@ -184,9 +184,6 @@ class PluginTester
 			@test 'finish up', (done) ->
 				done()
 				tester.exit()
-				if tester.config.autoExit isnt 'safe'
-					process.exit()
-					# ^ perhaps we can redo this with the new docpad.destroy() function
 
 		# Chain
 		@

src/lib/docpad.coffee
@@ -4239,8 +4239,9 @@ class DocPad extends EventEmitterGrouped
 				docpad.destroy (err) ->
 					# Check
 					if err
-						console.log(err.stack)
-						return process.exit(1)
+						process.stderr.write(err.stack or err.toString())  if err
+						# don't force exit, it should occur naturally
+						return
 
 					# Forward onto the local DocPad Instance now that it has been installed
 					return docpadUtil.startLocalDocPadExecutable()

src/lib/interfaces/console.coffee
@@ -246,10 +246,8 @@ class ConsoleInterface
 		# Destroy docpad
 		docpad.destroy (_err) ->
 			# Check for error
-			console.log(_err)  if _err
-
-			# Force shutdown now
-			process.exit(if err or _err then 1 else 0)
+			process.stderr.write(_err.stack or _err.toString())  if _err
+			# don't force exit, it should occur naturally
 
 		# Chain
 		@

src/lib/util.coffee
@@ -21,10 +21,9 @@ module.exports = docpadUtil =
 		command = ['node', docpadUtil.getLocalDocPadExecutable()].concat(args)
 		return require('safeps').spawn command, {stdio:'inherit'}, (err) ->
 			if err
-				console.log('An error occured in the child DocPad instance:', err.stack)
-				return process.exit(1)
-			else
-				return process.exit(0)
+				process.stderr.write('An error occured in the child DocPad instance:\n'+(err.stack or err.toString()))
+				# don't force exit, it should occur naturally
+			return
 
 	# get a filename without the extension
 	getBasename: (filename) ->

commit 97671412d97da1cc07c440d6894e37af0ca643e4

Fixed testers not exiting correctly - regression from 0913cae149e3268ab433c969303eeea251551fef

src/lib/testers.coffee
@@ -28,6 +28,7 @@ class PluginTester
 		outExpectedPath: null
 		removeWhitespace: false
 		contentRemoveRegex: null
+		autoExit: 'safe'
 
 	# DocPad Config
 	docpadConfig:
@@ -74,8 +75,8 @@ class PluginTester
 		joe.describe @config.testerName, (suite,task) ->
 			tester.describe = tester.suite = suite
 			tester.it = tester.test = task
-			tester.done = tester.exit = ->
-				tester.docpad?.destroy()
+			tester.done = tester.exit = (next) ->
+				tester.docpad?.destroy(next)
 			next?(null, tester)
 
 		# Chain
@@ -181,9 +182,8 @@ class PluginTester
 
 		# Finish
 		if tester.config.autoExit
-			@test 'finish up', (done) ->
-				done()
-				tester.exit()
+			@test 'finish up', (next) ->
+				tester.exit(next)
 
 		# Chain
 		@