docpad/docpad

commit e1410cf636cd49d5a352cf6e3bea25fd001cc068

Merged in a2e4502

src/lib/docpad.coffee
@@ -217,8 +217,10 @@ class DocPad extends EventEmitterGrouped
 		'contextualizeBefore'
 		'contextualizeAfter'
 		'renderBefore'
+		'renderCollectionBefore'
 		'render'                       # fired for each extension conversion
 		'renderDocument'               # fired for each document render, including layouts and render passes
+		'renderCollectionAfter'
 		'renderAfter'
 		'writeBefore'
 		'writeAfter'
@@ -3039,23 +3041,34 @@ class DocPad extends EventEmitterGrouped
 
 		# Render Collection
 		renderCollection = (collectionToRender,{renderPass},next) ->
-			# Prepare
-			subTasks = new TaskGroup().setConfig(concurrency:0).once('complete',next)
+			# Plugin Event
+			docpad.emitSerial 'renderCollectionBefore', {collection:collectionToRender,renderPass}, (err) =>
+				# Prepare
+				return next(err)  if err
 
-			# Cycle
-			step = "renderFiles (pass #{renderPass})"
-			opts.progress?.step(step).total(collectionToRender.length)
-			collectionToRender.forEach (file) ->
-				slowFilesObject[file.id] = file.get('relativePath')
-				subTasks.addTask (complete) ->
-					renderFile file, (err) ->
-						delete slowFilesObject[file.id] or file.id
-						opts.progress?.tick()
-						return complete(err)
+				subTasks = new TaskGroup().setConfig(concurrency:0).once 'complete', (err) ->
+					# Plugin Event
+					docpad.emitSerial 'renderCollectionAfter', {collection:collectionToRender,renderPass}, (err) ->
+						# Prepare
+						return next(err)  if err
 
-			# Return
-			subTasks.run()
-			return collectionToRender
+						# Done
+						return next()
+
+				# Cycle
+				step = "renderFiles (pass #{renderPass})"
+				opts.progress?.step(step).total(collectionToRender.length)
+				collectionToRender.forEach (file) ->
+					slowFilesObject[file.id] = file.get('relativePath')
+					subTasks.addTask (complete) ->
+						renderFile file, (err) ->
+							delete slowFilesObject[file.id] or file.id
+							opts.progress?.tick()
+							return complete(err)
+
+				# Return
+				subTasks.run()
+				return collectionToRender
 
 		# Plugin Event
 		docpad.emitSerial 'renderBefore', {collection,templateData}, (err) =>

commit 0862c44ab25bd3747b41cbfac182ea8ded88fe21

Added h5bp tests to catch skeleton regressions, like e2a89e2228d47c666af60381c79676e205b74a97. Fixed render errors not being reported, regression from e1410cf636cd49d5a352cf6e3bea25fd001cc068

src/lib/docpad.coffee
@@ -2400,7 +2400,6 @@ class DocPad extends EventEmitterGrouped
 
 		# Log
 		model.on 'log', (args...) ->
-			#debugger  if args[0] in ['warning', 'warn', 'err', 'error']
 			docpad.log(args...)
 
 		# Chain
@@ -3019,6 +3018,9 @@ class DocPad extends EventEmitterGrouped
 				return next(err)  if err
 
 				subTasks = new TaskGroup().setConfig(concurrency:0).once 'complete', (err) ->
+					# Prepare
+					return next(err)  if err
+
 					# Plugin Event
 					docpad.emitSerial 'renderCollectionAfter', {collection:collectionToRender,renderPass}, (err) ->
 						# Prepare

test/docpad.coffee
@@ -16,6 +16,38 @@ module.exports =
 	templateData:
 		require: require
 
+		site:
+			styles: ['/styles/style.css']
+			scripts: ['/scripts/script.js']
+			title: "Your Website"
+			description: """
+				When your website appears in search results in say Google, the text here will be shown underneath your website's title.
+				"""
+			keywords: """
+				place, your, website, keywoards, here, keep, them, related, to, the, content, of, your, website
+				"""
+
+		# Get the prepared site/document title
+		# Often we would like to specify particular formatting to our page's title
+		# we can apply that formatting here
+		getPreparedTitle: ->
+			# if we have a document title, then we should use that and suffix the site's title onto it
+			if @document.title
+				"#{@document.title} | #{@site.title}"
+			# if our document does not have it's own title, then we should just use the site's title
+			else
+				@site.title
+
+		# Get the prepared site/document description
+		getPreparedDescription: ->
+			# if we have a document description, then we should use that, otherwise use the site's description
+			@document.description or @site.description
+
+		# Get the prepared site/document keywords
+		getPreparedKeywords: ->
+			# Merge the document keywords with the site keywords
+			@site.keywords.concat(@document.keywords or []).join(', ')
+
 	collections:
 		docpadConfigCollection: (database) ->
 			database.findAllLive({tag: $has: 'docpad-config-collection'})

test/out-expected/h5bp-document.html
@@ -1,0 +1,48 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+	<!-- Standard Meta -->
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- Site Properities -->
+	<title>What now? | Your Website</title>
+	<meta name="description" content="When your website appears in search results in say Google, the text here will be shown underneath your website's title." />
+	<meta name="keywords" content="place, your, website, keywoards, here, keep, them, related, to, the, content, of, your, website" />
+
+	<!-- DocPad Meta -->
+	<meta name="generator" content="DocPad v6.54.0" />
+
+	<!-- DocPad Styles + Our Own -->
+	<link  rel="stylesheet" href="/styles/style.css" />
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+</head>
+<body>
+	<!--[if lt IE 7]>
+	<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+	<![endif]-->
+
+	<header></header>
+	<article>
+		<h2>Not sure what to do now? Here&#39;s some useful links:</h2>
+<ul>
+<li><a href="http://docpad.org/docs">DocPad Documentation</a></li>
+<li><a href="irc://irc.freenode.net/docpad">IRC Chat Room</a></li>
+</ul>
+
+	</article>
+	<footer></footer>
+
+	<!-- jQuery -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="/vendor/jquery.js"><\/script>')</script>
+
+	<!-- DocPad Scripts + Our Own -->
+	<script defer="defer"  src="/scripts/script.js"></script>
+</body>
+</html>

test/src/layouts/h5bp-layout.html.eco
@@ -1,0 +1,43 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+	<!-- Standard Meta -->
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- Site Properities -->
+	<title><%= @getPreparedTitle() %></title>
+	<meta name="description" content="<%= @getPreparedDescription() %>" />
+	<meta name="keywords" content="<%= @getPreparedKeywords() %>" />
+
+	<!-- DocPad Meta -->
+	<%- @getBlock('meta').toHTML() %>
+
+	<!-- DocPad Styles + Our Own -->
+	<%- @getBlock('styles').add(@site.styles).toHTML() %>
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+</head>
+<body>
+	<!--[if lt IE 7]>
+	<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+	<![endif]-->
+
+	<header></header>
+	<article>
+		<%- @content %>
+	</article>
+	<footer></footer>
+
+	<!-- jQuery -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="/vendor/jquery.js"><\/script>')</script>
+
+	<!-- DocPad Scripts + Our Own -->
+	<%- @getBlock('scripts').add(@site.scripts).toHTML() %>
+</body>
+</html>